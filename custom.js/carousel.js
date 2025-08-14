69612c7f0c9c646f7a30b01e9f6ecf8dc7ffd4c0
McChikenz <106206847+McChikenz@users.noreply.github.com>
2025-08-14 22:47:28 +0000

    Deploying to gh-pages from  @ 9064ed2f5d73966ad0baad34f649dc89bb2ad891 🚀

--- custom.js/carousel.js
+++ custom.js/carousel.js
@@ -1,29 +1,4 @@
-
   
-//   OLD CAROUSEL JS
-//   $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:10,
-//     nav:false,
-//     autoplay:true,
-//     // autoplayTimeout: 1000,
-//     loop:true,
-//     dots:false,
-//     // stagePadding: 50,
-//     responsive:{
-//         0:{
-//             items:1,
-//         },
-//         600:{
-//             items:3,
-//         },
-//         1000:{
-//             items:5,
-//         }
-//     }
-// })
-
-      
 // Owlcarousel
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
